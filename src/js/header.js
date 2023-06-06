e2d7882281c991b15bab402d5a96e1b1150e3f52
NinoPogi <ninoimmanoelbautista@gmail.com>
2023-06-06 17:58:26 +0800

    Added Cart

--- src/js/header.js
+++ src/js/header.js
@@ -18,5 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
       window.addEventListener("scroll", function () {
         header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
       });
+
+      const cartButton = document.querySelector("#cart-btn");
+      const cart = document.querySelector("[cart]");
+      const cartClose = document.querySelector("#cart-close");
+
+      cartButton.addEventListener("click", function () {
+        cart.classList.add("active");
+      });
+
+      cartClose.addEventListener("click", function () {
+        console.log(2);
+        cart.classList.remove("active");
+      });
     });
 });
